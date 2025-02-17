15f603daacd3eaad02cf7512edc1cdf339ca9724
alinaBeliolgo <159250463+alinaBeliolgo@users.noreply.github.com>
2025-02-17 20:48:53 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo "Hello world echo <br>";
+print "Hello world print <br>";
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+echo "Прошло " . $days . " дней. " . $message . "<br>";
+echo "Прошло $days дней. $message <br>";
+print "Прошло $days дней. $message <br>";
